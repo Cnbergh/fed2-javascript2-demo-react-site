608d318c2d2eeb3425aab19962b7cb9fd15eb092
Monde Sineke <nsineke@gmail.com>
2023-10-03 13:46:31 +0200

    Feat: Add noroff API helpers

--- src/lib/my-api.js
+++ src/lib/my-api.js
@@ -98,3 +98,55 @@ export const fetchThings = async (url) => {
     throw error;
   }
 };
+
+const NOROFF_API_URL = import.meta.env.VITE_NOROFF_API_URL;
+
+/**
+ * Helper function to add the
+ * @param {Object} options - HTTP header options
+ * @returns {Object} - HTTP header options with Authorization header
+ */
+function updateOptions(options) {
+  const update = { ...options };
+
+  if (localStorage.getItem("access_token")) {
+    update.headers = {
+      ...update.headers,
+      Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+    };
+  }
+
+  return update;
+}
+
+/**
+ * Wrapper around fetch to add Authorization header
+ * @returns {Promise} - fetch promise
+ */
+export default function fetcher(url, options) {
+  return fetch(url, updateOptions(options));
+}
+
+/**
+ * Fetch all posts with comments, reactions and the author
+ * @returns {Object | Error} - A list of posts
+ */
+export async function fetchNoroffPosts() {
+  const url = new URL(`${NOROFF_API_URL}/posts`);
+
+  url.searchParams.append("_author", "true");
+  url.searchParams.append("_comments", "true");
+  url.searchParams.append("_reactions", "true");
+
+  try {
+    const response = await fetcher(url.href);
+
+    if (!response.ok) throw new Error(response.statusText);
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    throw new Error(error);
+  }
+}

1e51c005793eaeed3186b01cb23e74e6f46ad0eb
Monde Sineke <nsineke@gmail.com>
2023-11-01 20:56:37 +0100

    test: :white_check_mark: new post assertions
    Create assertions

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,5 +1,29 @@
-describe("template spec", () => {
-  it("passes", () => {
+describe("Post page", () => {
+  it("navigates to a single post", () => {
     cy.visit("https://localhost:5173/");
+
+    cy.contains(
+      "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    ).click();
+
+    cy.url().should("include", "/posts/1");
+  });
+
+  it("creates a new post", () => {
+    cy.visit("https://localhost:5173/");
+    console.log(Cypress.env("CYPRESS_API_URL"));
+    cy.intercept("POST", `https://jsonplaceholder.typicode.com/posts`).as(
+      "new-post",
+    );
+
+    cy.get("#title").type("fake title");
+    cy.get("[name='userId']").type("123456");
+    cy.contains("Create Post").click();
+
+    cy.get("@new-post").should(
+      "have.nested.property",
+      "response.statusCode",
+      201,
+    );
   });
 });

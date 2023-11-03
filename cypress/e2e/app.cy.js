0ef162e0f6e066f7e991a96d8a8d483bd4db7d53
Monde Sineke <nsineke@gmail.com>
2023-11-03 11:10:31 +0100

    test: debugging test

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,10 +1,18 @@
 describe("Post page", () => {
   it("navigates to a single post", () => {
     cy.visit("https://localhost:5173/");
+    cy.intercept("GET", `https://jsonplaceholder.typicode.com/posts`).as(
+      "get-posts",
+    );
 
+    cy.debug();
+
+    cy.get("@get-post");
     cy.contains(
       "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-    ).click();
+    )
+      .click()
+      .debug();
 
     cy.url().should("include", "/posts/1");
   });
@@ -25,10 +33,10 @@ describe("Post page", () => {
       201,
     );
     /* ==== Generated with Cypress Studio ==== */
-    cy.get(':nth-child(1) > .flex > div > .bg-orange-700').click();
-    cy.get('#postEditBody').click();
-    cy.get('form > .text-white').click();
-    cy.get(':nth-child(2) > .flex > div > .bg-red-700').click();
+    cy.get(":nth-child(1) > .flex > div > .bg-orange-700").click();
+    cy.get("#postEditBody").click();
+    cy.get("form > .text-white").click();
+    cy.get(":nth-child(2) > .flex > div > .bg-red-700").click();
     /* ==== End Cypress Studio ==== */
   });
 });

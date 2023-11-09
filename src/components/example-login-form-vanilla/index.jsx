bc3bc4db466db2430b3e31d9481065d4002c225e
Monde Sineke <nsineke@gmail.com>
2023-11-09 18:31:38 +0100

    feat: :children_crossing: remove default password

--- src/components/example-login-form-vanilla/index.jsx
+++ src/components/example-login-form-vanilla/index.jsx
@@ -121,7 +121,6 @@ function ExampleSignInForm() {
                   type="password"
                   autoComplete="current-password"
                   required
-                  defaultValue="UzI1NiIsInR5cCI"
                   className="block w-full rounded-md border-0 px-1 py-1.5 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
